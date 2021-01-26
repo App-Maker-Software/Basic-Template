49a20ab6fa3951d11427ff4bb0328cd5ed39b019
Joe Hinkle <joehinkle11@gmail.com>
2021-01-26 16:03:14 -0600

    Revert "removed nav view"
    This reverts commit 288aee65c99016e0ea05d75aa727d1dab48ec0c7.

--- BasicTemplate/Views/AboutView.swift
+++ BasicTemplate/Views/AboutView.swift
@@ -9,57 +9,60 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Form {
-            Section(header: Text("Contact Us")) {
-                if AM_SHOW_TWITTER {
-                    Button(action: {
-                        UIApplication.shared.open(URL(string: AM_TWITTER_LINK)!)
-                    }, label: {
-                        HStack {
-                            Image("Twitter").resizable().frame(width: 24, height: 24)
-                            Text("Twitter")
-                        }
-                    })
+        NavigationView {
+            Form {
+                Section(header: Text("Contact Us")) {
+                    if AM_SHOW_TWITTER {
+                        Button(action: {
+                            UIApplication.shared.open(URL(string: AM_TWITTER_LINK)!)
+                        }, label: {
+                            HStack {
+                                Image("Twitter").resizable().frame(width: 24, height: 24)
+                                Text("Twitter")
+                            }
+                        })
+                    }
+                    if AM_SHOW_INSTAGRAM {
+                        Button(action: {
+                            UIApplication.shared.open(URL(string: AM_INSTAGRAM_LINK)!)
+                        }, label: {
+                            HStack {
+                                Image("Instagram").resizable().frame(width: 24, height: 24)
+                                Text("Instagram")
+                            }
+                        })
+                    }
+                    if AM_SHOW_SOUNDCLOUD {
+                        Button(action: {
+                            UIApplication.shared.open(URL(string: AM_SOUNDCLOUD_LINK)!)
+                        }, label: {
+                            HStack {
+                                Image("SoundCloud").resizable().frame(width: 24, height: 24)
+                                Text("SoundCloud")
+                            }
+                        })
+                    }
+                    if AM_SHOW_WEBSITE {
+                        Button(action: {
+                            UIApplication.shared.open(URL(string: AM_WEBSITE_LINK)!)
+                        }, label: {
+                            HStack {
+                                Image(systemName: "link.circle")
+                                Text("Our Website")
+                            }
+                        })
+                    }
                 }
-                if AM_SHOW_INSTAGRAM {
-                    Button(action: {
-                        UIApplication.shared.open(URL(string: AM_INSTAGRAM_LINK)!)
-                    }, label: {
-                        HStack {
-                            Image("Instagram").resizable().frame(width: 24, height: 24)
-                            Text("Instagram")
-                        }
-                    })
+                Section(header: Text("Info")) {
+                    Text(AM_APP_DESCRIPTION)
+                    Text("Created in ") + Text(AM_YEAR_CREATED).bold()
                 }
-                if AM_SHOW_SOUNDCLOUD {
-                    Button(action: {
-                        UIApplication.shared.open(URL(string: AM_SOUNDCLOUD_LINK)!)
-                    }, label: {
-                        HStack {
-                            Image("SoundCloud").resizable().frame(width: 24, height: 24)
-                            Text("SoundCloud")
-                        }
-                    })
-                }
-                if AM_SHOW_WEBSITE {
-                    Button(action: {
-                        UIApplication.shared.open(URL(string: AM_WEBSITE_LINK)!)
-                    }, label: {
-                        HStack {
-                            Image(systemName: "link.circle")
-                            Text("Our Website")
-                        }
-                    })
-                }
+                VStack(alignment: .center) {
+                    PoweredByAppMakerView()
+                }.padding().frame(maxWidth: .infinity)
             }
-            Section(header: Text("Info")) {
-                Text(AM_APP_DESCRIPTION)
-                Text("Created in ") + Text(AM_YEAR_CREATED).bold()
-            }
-            VStack(alignment: .center) {
-                PoweredByAppMakerView()
-            }.padding().frame(maxWidth: .infinity)
-        }
+            .navigationBarTitle("About", displayMode: .large)
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
